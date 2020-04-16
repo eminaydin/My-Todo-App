0593568b7bda4b5bab87d8ca266d74e37e125577
Emin Aydın <eminaydin93@yahoo.com>
2020-04-16 20:20:25 +0200

    Closing functionality added

--- script.js
+++ script.js
@@ -1,66 +1,58 @@
 let toDoItems = [];
 const form = document.querySelector("form");
 const input = document.querySelector(".todo");
-const list = document.querySelector(".todo-list")
-
+const list = document.querySelector(".todo-list");
 
 addToDO = (text) => {
-const toDo = {
+  const toDo = {
     text,
     checked: false,
     id: Date.now(),
-};
-toDoItems.push(toDo);
-console.log(toDoItems); 
+  };
+  toDoItems.push(toDo);
+  console.log(toDoItems);
 
-list.insertAdjacentHTML('beforeend', `
-<li data-key="${toDo.id}" class = "todo-li">
-<label class = "todo-label">
-        <input type="checkbox" class="filled-in" />
-        <span class="todo-item"> <span class = "todo-text"> ${toDo.text}</span></span>
-        <span class="material-icons close">
-  close
+  list.insertAdjacentHTML(
+    "beforeend",
+    `
+<li data-key="${toDo.id}" class ="todo-li">
+   <label for=${toDo.id} class="tick js-tick"> </label>
+ <input id="${toDo.id}" type="checkbox"/>
+ <span>${toDo.text}</span>
+ <span class="material-icons close">
+   close
   </span>
-      </label>
-
-
   <li/>
-` 
-);
+`
+  );
+};
+function toggleDone(key) {
+  const index = toDoItems.findIndex((item) => item.id === Number(key));
 
 }
+function deleteTodo(key) {
+  toDoItems = toDoItems.filter(item => item.id !== Number(key));
+  const item = document.querySelector(`[data-key='${key}']`);
+  item.remove();
+}
 
-function toggleDone(key) {
-    const index = toDoItems.findIndex(item => item.id === (key));
-    toDoItems[index].checked = !toDoItems[index].checked;
-  
-    const item = document.querySelector(`[data-key='${key}']`);
-    if (toDoItems[index].checked) {
-      item.classList.add('done');
-      console.log("chedced");
-      
-    } else {
-      item.classList.remove('done');
-    }
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const item = input.value.trim();
+  if (item !== "") {
+    addToDO(item);
+    input.value = "";
+    input.focus();
   }
-
-
-
-form.addEventListener("submit", e => {
-    e.preventDefault();
-    const item = input.value.trim();
-    if (item !== "") {
-        addToDO(item);
-        input.value = "";
-        input.focus();
-    }
 });
 
-list.addEventListener('click', event => {
-    if (event.target.classList.contains('todo-li')) {
-      const itemKey = event.target.parentElement.dataset.key;
-      toggleDone(itemKey);
-      console.log("askdaskda");
-      
-    }
-  });
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("tick")) {
+    const itemKey = event.target.parentElement.dataset.key;
+    toggleDone(itemKey);
+  }
+  if (event.target.classList.contains('close')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    deleteTodo(itemKey);
+  }
+});
