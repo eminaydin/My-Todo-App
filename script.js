47d513efdec9c9ce9463eabe32e5b5dcacbf50e0
Emin Aydın <eminaydin93@yahoo.com>
2020-04-16 14:19:39 +0200

    styles changed

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let toDoItems = [];
 const form = document.querySelector("form");
 const input = document.querySelector(".todo");
-const newList = document.createElement("ul");
+const list = document.querySelector(".todo-list")
+
+
 addToDO = (text) => {
 const toDo = {
     text,
@@ -9,10 +11,40 @@ const toDo = {
     id: Date.now(),
 };
 toDoItems.push(toDo);
-console.log(toDoItems);
-newList.innerHTML = `
-`
+console.log(toDoItems); 
+
+list.insertAdjacentHTML('beforeend', `
+<li data-key="${toDo.id}" class = "todo-li">
+<label class = "todo-label">
+        <input type="checkbox" class="filled-in" />
+        <span class="todo-item"> <span class = "todo-text"> ${toDo.text}</span></span>
+        <span class="material-icons close">
+  close
+  </span>
+      </label>
+
+
+  <li/>
+` 
+);
+
 }
+
+function toggleDone(key) {
+    const index = toDoItems.findIndex(item => item.id === (key));
+    toDoItems[index].checked = !toDoItems[index].checked;
+  
+    const item = document.querySelector(`[data-key='${key}']`);
+    if (toDoItems[index].checked) {
+      item.classList.add('done');
+      console.log("chedced");
+      
+    } else {
+      item.classList.remove('done');
+    }
+  }
+
+
 
 form.addEventListener("submit", e => {
     e.preventDefault();
@@ -22,4 +54,13 @@ form.addEventListener("submit", e => {
         input.value = "";
         input.focus();
     }
-})
+});
+
+list.addEventListener('click', event => {
+    if (event.target.classList.contains('todo-li')) {
+      const itemKey = event.target.parentElement.dataset.key;
+      toggleDone(itemKey);
+      console.log("askdaskda");
+      
+    }
+  });
