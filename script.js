e4c7967487449bbb4a3d2c8cdf73ff5fbf885db4
Emin Aydın <eminaydin93@yahoo.com>
2020-04-16 11:04:45 +0200

    Getting inputs from user in console

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let toDoItems = [];
-
+const form = document.querySelector("form");
+const input = document.querySelector(".todo");
+const newList = document.createElement("ul");
 addToDO = (text) => {
 const toDo = {
     text,
@@ -7,5 +9,17 @@ const toDo = {
     id: Date.now(),
 };
 toDoItems.push(toDo);
-
+console.log(toDoItems);
+newList.innerHTML = `
+`
 }
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    const item = input.value.trim();
+    if (item !== "") {
+        addToDO(item);
+        input.value = "";
+        input.focus();
+    }
+})
