18be2fa3b267b0ba35d29f5de8538896aef165a5
Emin Aydın <eminaydin93@yahoo.com>
2020-04-17 14:03:40 +0200

    less .js included

--- script.js
+++ script.js
@@ -14,22 +14,30 @@ addToDO = (text) => {
     "beforeend",
     `
 <li data-key="${toDo.id}" class ="todo-li">
-   <label for=${toDo.id} class="tick js-tick"> </label>
- <input id="${toDo.id}" type="checkbox"/>
- <span>${toDo.text}</span>
+<input id="${toDo.id}" type="checkbox" class="checkbox"/>
+<label for=${toDo.id} class="tick js-tick"> </label>
+ <span class="todo-text">${toDo.text}</span>
  <span class="material-icons close">
    close
   </span>
+  <span></span>
   <li/>
 `
   );
 };
 function toggleDone(key) {
   const index = toDoItems.findIndex((item) => item.id === Number(key));
+  toDoItems[index].checked = !toDoItems[index].checked;
+  const item = document.querySelector(".todo-text");
+  if (toDoItems[index].checked) {
+    item.classList.add("done");
+  } else {
+    item.classList.remove("done");
+  }
 }
 function deleteTodo(key) {
-  toDoItems = toDoItems.filter(item => item.id !== Number(key));
-  const item = document.querySelector(`[data-key='${key}']`);
+  toDoItems = toDoItems.filter((item) => item.id !== Number(key));
+  const item = document.querySelector(".todo-text");
   item.remove();
 }
 
@@ -45,11 +53,12 @@ form.addEventListener("submit", (e) => {
 });
 
 list.addEventListener("click", (e) => {
-  if (e.target.classList.contains("tick")) {
+  if (e.target.classList.contains("checkbox")) {
     const itemKey = event.target.parentElement.dataset.key;
+
     toggleDone(itemKey);
   }
-  if (event.target.classList.contains('close')) {
+  if (event.target.classList.contains("close")) {
     const itemKey = event.target.parentElement.dataset.key;
     deleteTodo(itemKey);
   }
