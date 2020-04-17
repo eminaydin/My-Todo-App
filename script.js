f2f615c4685bb8c85dd6dad93e044a5e1b9648a1
Emin Aydın <eminaydin93@yahoo.com>
2020-04-17 17:19:51 +0200

    Simple working version completed

--- script.js
+++ script.js
@@ -1,43 +1,44 @@
 let toDoItems = [];
 const form = document.querySelector("form");
 const input = document.querySelector(".todo");
-const list = document.querySelector(".todo-list");
+const list = document.querySelector(".list");
 
 addToDO = (text) => {
   const toDo = {
     text,
-    checked: false,
     id: Date.now(),
   };
   toDoItems.push(toDo);
   list.insertAdjacentHTML(
     "beforeend",
     `
-<li data-key="${toDo.id}" class ="todo-li">
-<input id="${toDo.id}" type="checkbox" class="checkbox"/>
-<label for=${toDo.id} class="tick js-tick"> </label>
- <span class="todo-text">${toDo.text}</span>
- <span class="material-icons close">
-   close
-  </span>
-  <span></span>
-  <li/>
+  <li class="list-item" data-key="${toDo.id}">
+  <span class="close">x</span>
+  <input type="checkbox" class="hidden-box" id=${toDo.id}/>
+  <label for=${toDo.id} class="check--label">
+    <span class="check--label-box"></span>
+    <span class="check--label-text">${toDo.text}</span>
+    
+  </label>
+</li>
 `
   );
 };
 function toggleDone(key) {
   const index = toDoItems.findIndex((item) => item.id === Number(key));
   toDoItems[index].checked = !toDoItems[index].checked;
-  const item = document.querySelector(".todo-text");
+
+  const item = document.querySelector(`.hidden-box`);
   if (toDoItems[index].checked) {
-    item.classList.add("done");
+    item.setAttribute("checked", "");
   } else {
-    item.classList.remove("done");
+    item.removeAttribute("checked", "");
   }
 }
+
 function deleteTodo(key) {
   toDoItems = toDoItems.filter((item) => item.id !== Number(key));
-  const item = document.querySelector(".todo-text");
+  const item = document.querySelector(`[data-key="${key}"]`);
   item.remove();
 }
 
@@ -52,14 +53,15 @@ form.addEventListener("submit", (e) => {
   }
 });
 
-list.addEventListener("click", (e) => {
-  if (e.target.classList.contains("checkbox")) {
-    const itemKey = event.target.parentElement.dataset.key;
-
+list.addEventListener("click", (event) => {
+  if (event.target.classList.contains("check--label-box")) {
+    const itemKey = event.target.parentElement.parentElement.dataset.key;
     toggleDone(itemKey);
   }
+
   if (event.target.classList.contains("close")) {
     const itemKey = event.target.parentElement.dataset.key;
+
     deleteTodo(itemKey);
   }
 });
