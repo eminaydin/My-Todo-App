ffe4db5aa404747b7dd032274870faa0260172fd
Emin <eminaydin93@yahoo.com>
2020-04-18 20:11:13 +0200

    Icon added, minor bugs fixed

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ addToDO = (text) => {
   <label for=${toDo.id} class="check--label">
     <span class="check--label-box"></span>
     <span class="check--label-text">${toDo.text}</span>
-    
   </label>
 </li>
 `
@@ -27,23 +26,22 @@ addToDO = (text) => {
 function toggleDone(key) {
   const index = toDoItems.findIndex(item => item.id === Number(key));
   toDoItems[index].checked = !toDoItems[index].checked;
-
   const item = document.querySelector(`.hidden-box`);
   if (toDoItems[index].checked) {
-    item.setAttribute("checked","");
+    item.setAttribute("checked", "");
+    console.log(toDoItems[index]);
+
   }
 }
 
 function deleteTodo(key) {
   toDoItems = toDoItems.filter((item) => item.id !== Number(key));
-  const item = document.querySelector(`[data-key="${key}"]`);  
+  const item = document.querySelector(`[data-key="${key}"]`);
   item.remove();
 }
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  document.querySelector("#hint").classList.remove("todo-hint");
-  document.querySelector("#hint").classList.add("animated", "bounceOutLeft","visible");
   const item = input.value.trim();
   if (item !== "") {
     addToDO(item);
@@ -54,13 +52,12 @@ form.addEventListener("submit", (e) => {
 
 list.addEventListener("click", (event) => {
   if (event.target.classList.contains('check--label-box')) {
-    const itemKey = event.target.parentElement.parentElement.dataset.key;
+    const itemKey = event.target.parentElement.dataset.key;
     toggleDone(itemKey);
   }
 
   if (event.target.classList.contains("close")) {
     const itemKey = event.target.parentElement.dataset.key;
-  
     deleteTodo(itemKey);
   }
 });
