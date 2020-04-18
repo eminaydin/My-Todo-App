641fa3417ff745ca2aeda70dc3edbd618cfe0b28
Emin <eminaydin93@yahoo.com>
2020-04-19 00:43:47 +0200

    Line comments and animation on closing added

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ addToDO = (text) => {
     id: Date.now(),
   };
   toDoItems.push(toDo);
+  // Append list structure to ul element
   list.insertAdjacentHTML(
     "beforeend",
     `
@@ -23,14 +24,13 @@ addToDO = (text) => {
 `
   );
 };
+
 function toggleDone(key) {
   const index = toDoItems.findIndex(item => item.id === Number(key));
   toDoItems[index].checked = !toDoItems[index].checked;
   const item = document.querySelector(`.hidden-box`);
   if (toDoItems[index].checked) {
     item.setAttribute("checked", "");
-    console.log(toDoItems[index]);
-
   }
 }
 
@@ -39,7 +39,7 @@ function deleteTodo(key) {
   const item = document.querySelector(`[data-key="${key}"]`);
   item.remove();
 }
-
+// On submit, cut the whitespaces and if its input is not empty add it to list and clear input field 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const item = input.value.trim();
@@ -49,15 +49,19 @@ form.addEventListener("submit", (e) => {
     input.focus();
   }
 });
-
+//Check if click happens on checkbox, if so do the magic
 list.addEventListener("click", (event) => {
   if (event.target.classList.contains('check--label-box')) {
     const itemKey = event.target.parentElement.dataset.key;
     toggleDone(itemKey);
   }
-
+  // Close with animation when click happens on close button
   if (event.target.classList.contains("close")) {
+    document.querySelector("li").className = "animated slideOutRight";
     const itemKey = event.target.parentElement.dataset.key;
-    deleteTodo(itemKey);
+    setTimeout(() => {
+      deleteTodo(itemKey);
+    }, 700);
+
   }
 });
