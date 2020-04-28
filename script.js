d7219144b068ea1ae0c14779ed9368ff9d9134e1
Emin <eminaydin93@yahoo.com>
2020-04-28 23:11:28 +0200

    Typo and style fixes

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const input = document.querySelector(".todo");
 const list = document.querySelector(".list");
 const body = document.querySelector("body");
 
-
 addToDO = (text) => {
   const toDo = {
     text,
@@ -17,7 +16,6 @@ addToDO = (text) => {
     "beforeend",
     `
   <li class="list-item animated slideInLeft" data-key="${toDo.id}">
-  
   <input type="checkbox" class="hidden-box" id="${toDo.id}"/>
   <label for=${toDo.id} class="check--label">
     <span class="check--label-box"></span>
@@ -36,7 +34,9 @@ function deleteTodo(key) {
 // On submit, cut the whitespaces of given input and if it's not empty add it to list and clear input field
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  body.style.height = 100 + "%";
+  const style = body.style;
+  style.height = 100 + "%";
+  style.marginTop = 10 + "px";
   const item = input.value.trim();
   if (item !== "") {
     addToDO(item);
@@ -44,13 +44,10 @@ form.addEventListener("submit", (e) => {
     input.focus();
   }
 });
+//delete the item if close button clicked
 list.addEventListener("click", (event) => {
-  // Close with animation when click happens on close button
   if (event.target.classList.contains("close")) {
-    document.getElementsByClassName(".list-item").className =
-      "animated slideOutRight";
-    console.log(document.getElementsByClassName(".list-item").className);
-    const itemKey = event.target.parentElement.dataset.key;
+    const itemKey = event.target.parentElement.parentElement.dataset.key;
     deleteTodo(itemKey);
     console.log(itemKey);
   }
