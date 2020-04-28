6fd0ae8b67c5dc23e88523617e60c056aecd0888
Emin <eminaydin93@yahoo.com>
2020-04-28 22:39:43 +0200

    Responsive design added

--- script.js
+++ script.js
@@ -2,77 +2,56 @@ let toDoItems = [];
 const form = document.querySelector("form");
 const input = document.querySelector(".todo");
 const list = document.querySelector(".list");
+const body = document.querySelector("body");
+
 
 addToDO = (text) => {
   const toDo = {
     text,
     id: Date.now(),
   };
+
   toDoItems.push(toDo);
   // Append list structure to ul element
   list.insertAdjacentHTML(
     "beforeend",
     `
   <li class="list-item animated slideInLeft" data-key="${toDo.id}">
-  <span class="close">x</span>
+  
   <input type="checkbox" class="hidden-box" id="${toDo.id}"/>
   <label for=${toDo.id} class="check--label">
     <span class="check--label-box"></span>
     <span class="check--label-text">${toDo.text}</span>
+    <span class="close">x</span>
   </label>
 </li>
 `
   );
 };
 
-function toggleDone(key) {
-  const index = toDoItems.findIndex(item => item.id === Number(key));
-  toDoItems[index].checked = !toDoItems[index].checked;
-  const item = document.querySelector(`.hidden-box`);
-  if (toDoItems[index].checked) {
-    item.setAttribute("checked", "");
-  }
-}
-
 function deleteTodo(key) {
-  toDoItems = toDoItems.filter((item) => item.id !== Number(key));
   const item = document.querySelector(`[data-key="${key}"]`);
   item.remove();
 }
-// On submit, cut the whitespaces and if its input is not empty add it to list and clear input field 
+// On submit, cut the whitespaces of given input and if it's not empty add it to list and clear input field
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  body.style.height = 100 + "%";
   const item = input.value.trim();
   if (item !== "") {
     addToDO(item);
     input.value = "";
     input.focus();
-
   }
 });
-//Check if click happens on checkbox, if so do the magic
 list.addEventListener("click", (event) => {
-  if (event.target.classList.contains('check--label-box')) {
-    const itemKey = event.target.parentElement.dataset.key;
-    toggleDone(itemKey);
-
-  }
   // Close with animation when click happens on close button
   if (event.target.classList.contains("close")) {
-<<<<<<< HEAD
-
+    document.getElementsByClassName(".list-item").className =
+      "animated slideOutRight";
     console.log(document.getElementsByClassName(".list-item").className);
-
     const itemKey = event.target.parentElement.dataset.key;
     deleteTodo(itemKey);
-
-=======
-    document.querySelector("li").className = "animated slideOutRight";
-    const itemKey = event.target.parentElement.dataset.key;
-    setTimeout(() => {
-      deleteTodo(itemKey);
-    }, 700);
->>>>>>> 641fa3417ff745ca2aeda70dc3edbd618cfe0b28
-
+    console.log(itemKey);
   }
 });
